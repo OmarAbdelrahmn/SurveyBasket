361e4e01832d1990dcf8589a858e8c287023e147
omar <omarfaroq2003@gmail.com>
2025-03-16 23:03:55 +0200

    add get roles endpoint

---
+++ SurveyBasket/Controllers/RolesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveyBasket.Services.Roles;
+
+namespace SurveyBasket.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RolesController(IRoleService roleService) : ControllerBase
+{
+    private readonly IRoleService roleService = roleService;
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAllRoles([FromQuery] bool IncludeDisable)
+    {
+        var response = await roleService.GetRolesAsync(IncludeDisable);
+
+        return response is not null ?
+            Ok(response.Value) :
+            response!.ToProblem();
+
+    }
+
+}
