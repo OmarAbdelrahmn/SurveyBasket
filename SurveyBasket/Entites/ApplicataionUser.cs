004c642ab069b831ea8f91cba16f001ffdb68fee
omar <omarfaroq2003@gmail.com>
2025-03-17 17:23:52 +0200

    extend user table

--- SurveyBasket/Entites/ApplicataionUser.cs
+++ SurveyBasket/Entites/ApplicataionUser.cs
@@ -4,6 +4,7 @@ public sealed class ApplicataionUser : IdentityUser
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public bool IsDisable { get; set; }
     public List<RefreshToken> RefreshTokens { get; set; } = [];
 
 }
---
+++ SurveyBasket/Migrations/20250317152143_addDisableusers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyBasket.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDisableusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisable",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDisable",
+                table: "AspNetUsers");
+
+        }
+    }
+}
